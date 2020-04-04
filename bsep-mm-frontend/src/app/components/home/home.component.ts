f9e328a96e49a55c00d6af36ad590e41b9193658
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2020-04-04 19:40:05 +0200

    Initialized spring and angular with keycloak

---
+++ bsep-mm-frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+import { DummyService } from 'src/app/services/dummy.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  username: string;
+  serverResp: string;
+  numOfRequests: number = 0;
+
+  constructor(private keyCloakSvc: KeycloakService, private ds: DummyService) { }
+
+  ngOnInit() {
+    this.username = this.keyCloakSvc.getUsername();
+  }
+
+  logout() {
+    this.keyCloakSvc.logout();
+  }
+
+  getHello() {
+    this.numOfRequests += 1;
+    this.ds.getHello().subscribe(
+      data => {
+        this.serverResp = data;
+      },
+      err => {
+        this.serverResp = err.error;
+      }
+    );
+  }
+
+}
---
+++ bsep-mm-frontend/src/environments/environment.ts
@@ -0,0 +1,7 @@
+
+export const environment = {
+  production: false,
+  KEYCLOAK_URL: 'http://localhost:8080/auth',
+  KEYCLOAK_REALM: 'BSEP-Military-Monitoring',
+  KEYCLOAK_CLIENTID: 'bsep-mm-frontend'
+};
