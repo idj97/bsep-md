0328f556cc1a649c71dcdec507b7c0bcf254d5db
Milos Malidza <33932996+milosmalidza@users.noreply.github.com>
2020-04-07 17:55:24 +0200

    Initial PKI front design

--- bsep-mm-frontend/src/app/app-routing.module.ts
+++ bsep-mm-frontend/src/app/app-routing.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NewCertificateComponent } from './components/new-certificate/new-certificate.component';
+import { IssuedCertificatesComponent } from './components/issued-certificates/issued-certificates.component';
+import { CertificateRequestsComponent } from './components/certificate-requests/certificate-requests.component';
 
 
 const routes: Routes = [
@@ -12,7 +15,19 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'new-certificate',
+    component: NewCertificateComponent
+  },
+  {
+    path: 'issued-certificates',
+    component: IssuedCertificatesComponent
+  },
+  {
+    path: 'certificate-requests',
+    component: CertificateRequestsComponent
+  },
 
 ];
 
---
+++ bsep-mm-frontend/src/app/components/issued-certificates/issued-certificates.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issued-certificates',
+  templateUrl: './issued-certificates.component.html',
+  styleUrls: ['./issued-certificates.component.css']
+})
+export class IssuedCertificatesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
