b4d1585b160a6a940a53a7f5abfd0bc89c031663
Bojan Vlasonjic <bojanvlasonjic@yahoo.com>
2020-04-08 21:13:24 +0200

    Added view of certificate requests for admin

--- bsep-mm-frontend/src/app/components/certificate-requests/certificate-requests.component.ts
+++ bsep-mm-frontend/src/app/components/certificate-requests/certificate-requests.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CertificateSignRequest } from 'src/app/dtos/CertificateSignRequest.dto';
 
 @Component({
   selector: 'app-certificate-requests',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CertificateRequestsComponent implements OnInit {
 
-  constructor() { }
+  certificateSignRequests: Array<CertificateSignRequest>;
+
+  constructor() {
+    this.certificateSignRequests = [];
+    
+    /*
+    let csr = new CertificateSignRequest();
+    csr.city = 'Lawrenceville';
+    csr.commonName = 'Some common name';
+    csr.country = 'USA, Georgia';
+    csr.email = 'somecsr@someemail.com';
+    csr.organisation = 'Some organisation';
+    csr.organisationUnit = 'Some organisation unit ';
+
+    this.certificateSignRequests.push(csr);
+    this.certificateSignRequests.push(csr);
+    this.certificateSignRequests.push(csr);*/
+
+  }
 
   ngOnInit() {
+  }
+
+  getCertificateRequests() {
+
+  }
+
+  approveCertificateRequest() {
+
+  }
+
+  declineCertificateRequest() {
+
   }
 
 }
---
+++ bsep-mm-frontend/src/app/dtos/CertificateSignRequest.dto.ts
@@ -0,0 +1,15 @@
+
+export class CertificateSignRequest {
+
+    commonName: string;
+    organisation: string;
+    organisationUnit: string;
+    city: string;
+    country: string;
+    email: string;
+    publicKey: string;
+
+    constructor() {
+        
+    }
+}
